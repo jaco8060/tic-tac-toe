d4fb24ab9c05a4919bd8019a1032b3fc5dece9c6
Jeremiah <jeremiahjacobson10@gmail.com>
2024-01-20 14:56:48 -0500

    added printBoard method to Gameboard

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 //IIFE single instance gameboard function
 const Gameboard = (function () {
-      const rows = 3;
-      const cols = 3;
-      const board = [];
-    
-      for (let i = 0; i < rows; i++) {
-        board[i] = []; //create an array for each row
-        for (let j = 0; j < cols; j++) {
-          board[i].push(Cell()); //place a cell object in each cell and append cell to array
-        }
-        
-        const getBoard = () => board;
-    
-        }
+  const rows = 3;
+  const cols = 3;
+  const board = [];
 
+  for (let i = 0; i < rows; i++) {
+    board[i] = []; //create an array for each row
+    for (let j = 0; j < cols; j++) {
+      board[i].push(Cell()); //place a cell object in each cell and append cell to array
+    }
+  }
+  const getBoard = () => board;
+
+  const printBoard = () => {
+    const boardWithValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.log(boardWithValues);
+  };
+
+  return { getBoard, printBoard };
 })();
 
-}
 // A cell represents a square on the board and has the value of the player assigned to it. If Cell value is 0 (default) then no moves were made on that cell, and if the cell is 1 then player 1's move has made
 function Cell() {
   let value = 0;
